f92c2ccbd87b1592075230fcb8a1cb476d4a059f
antoineartbox <antoine.fortin@artbox.agency>
2017-10-19 16:55:07 -0400

    Adding video display toggle on main page

--- dist/assets/js/main-ux-ui.js
+++ dist/assets/js/main-ux-ui.js
@@ -2,9 +2,6 @@
 $( document ).ready(function() {
 
 // Code to be executed when the whole document object model has loaded
-
-
-
     // Handle closing toggling membership menu
     $(".become-member-container .button").click(function() {
 
@@ -18,6 +15,7 @@ $( document ).ready(function() {
         });
     })
 
+    // Handle closing of involve
     $(".become-involve").click(function() {
         $("#bloc-involve-overlay").addClass("active-involving");
         blockScrolling('body')
@@ -30,8 +28,36 @@ $( document ).ready(function() {
     });
 
 
+    // Handle video
+    $(".button.button-video").click(function() {
+        displayVideo(true);
+
+        $(".closing-button.video").click(function() {
+            displayVideo(false);
+        });
+
+    });
+
 });
 
+
+function displayVideo(active) {
+    var height;
+    if(!active) {
+        height = "0%";
+    } else {
+        height = "100%";
+    }
+
+    $(".video-foreground iframe").animate({
+        "height" : height
+    }, 1000, function() {
+        // This function is trigger when the animation is done
+
+        // Display Button for closing video
+        $(".closing-button.video").addClass("active-button-video");
+    });
+}
 
 function blockScrolling(element) {
     $(element).css({
--- dist/site-parts/bloc-video.php
+++ dist/site-parts/bloc-video.php
@@ -1,5 +1,6 @@
 <div id="bloc-video" class="bloc kill-video">
     <div class="video-background">
+        <span class="closing-button video">x</span>
         <div class="video-foreground">
           <iframe src="https://www.youtube.com/embed/W0LHTWG-UmQ?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=W0LHTWG-UmQ" frameborder="0" allowfullscreen></iframe>
         </div>
