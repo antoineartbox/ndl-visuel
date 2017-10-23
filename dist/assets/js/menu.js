5a6bc8c3a099996d32098e0552890223eeb890be
antoineartbox <antoine.fortin@artbox.agency>
2017-10-23 13:35:37 -0400

    Adding main bloc styling

--- dist/assets/js/menu.js
+++ dist/assets/js/menu.js
@@ -1,4 +1,4 @@
-// Data structures as [0] => blocInfos, [1] => "color"
+// Define variables
 var blocInfos = [$("#bloc-infos").offset().top, "blue", "bloc-infos"];
 var blocEvents = [$("#bloc-events").offset().top, "transparent", "bloc-event"];
 var blocInvolve = [$("#bloc-involve").offset().top, "transparent", "bloc-involve"];
@@ -6,24 +6,25 @@ var blocInvolveInfos = [$("#bloc-involve-infos").offset().top, "blue", "bloc-inv
 var blocQuestions = [$("#bloc-question").offset().top, "transparent", "block-question"]
 var blocs = [blocInfos, blocEvents, blocInvolve,blocInvolveInfos, blocQuestions];
 
-$(window).scroll(function() {
 
+// Define user behaviour
+$(window).scroll(function() {
     var currentScroll = $(window).scrollTop();
     // Loop through all blocks
     for (var i = 0; i < blocs.length; i++) {
         var currentBlock = blocs[i];
         if (currentScroll >= currentBlock[0] && currentScroll < currentBlock[0] + 500) {
-            displayMenuBackground(currentBlock)
+            displayHeaderBackground(currentBlock)
         }
     }
 });
 
+// Define functions
+function displayHeaderBackground(blocInfos) {
 
-function displayMenuBackground(blocInfos) {
-    console.log(blocInfos);
     if (blocInfos[1] == "blue") {
-        $('#menu-container').css({"background-color":"white"});
+        $('header').css({"background-color":"white"});
     } else {
-        $('#menu-container').css({"background-color":"transparent"});
+        $('header').css({"background-color":"transparent"});
     }
 }
--- dist/site-parts/bloc-infos.php
+++ dist/site-parts/bloc-infos.php
@@ -1,35 +1,48 @@
 <div id="bloc-infos" class="bloc blue">
-    <div class="bloc-title-center">
-        <h3>Ancien élève de NDL ? Notre groupe Alumni s’adresse à vous ! </h3>
-        <span class="underline"></span>
-    </div>
 
-    <div class="bloc-infos-container">
-        <div class="text-right-container">
-            <p>Nous vous invitons à contribuer à la notoriété du collège en vous impliquant dans des projets novateurs et inspirants tout en profitant d’événements exclusifs. Une belle façon de renforcer votre réseau professionnel et personnel et de rencontrer de talentueux étudiants et parents de la cohorte actuelle.</p>
-        </div>
-    </div>
-    <div class="bloc-infos-bullet-container">
-        <div class="bullet-right-container">
-            <h3>Le programme Alumni c’est...</h3>
+    <div class="content-container">
 
-            <div class="bullet-list-container">
-                <ul>
-                    <li class="first">-Des rencontres enrichissantes</li>
-                    <li>-Des événements exclusifs</li>
-                    <li>-Un programme ambitieux</li>
-                    <li>-Une approche différente</li>
-                    <li class="last">-S’impliquer en redonnant</li>
-                </ul>
+        <div class="line-wrapper">
+            <div class="bloc-title-center">
+                <h3>Ancien élève de NDL ? Notre groupe Alumni s’adresse à vous ! </h3>
+                <span class="underline"></span>
             </div>
+            <div class="dual-block-container">
+                <div class="group-left">
+                    <div class="bloc-infos-container">
+                        <div class="text-right-container">
+                            <p>Nous vous invitons à contribuer à la notoriété du collège en vous impliquant dans des projets novateurs et inspirants tout en profitant d’événements exclusifs. Une belle façon de renforcer votre réseau professionnel et personnel et de rencontrer de talentueux étudiants et parents de la cohorte actuelle.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="group-right">
+                    <div class="bloc-infos-bullet-container">
+                        <div class="bullet-right-container">
+                            <h3>Le programme Alumni c’est...</h3>
+                            <div class="bullet-list-container">
+                                <ul>
+                                    <li class="first">-Des rencontres enrichissantes</li>
+                                    <li>-Des événements exclusifs</li>
+                                    <li>-Un programme ambitieux</li>
+                                    <li>-Une approche différente</li>
+                                    <li class="last">-S’impliquer en redonnant</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
+
+
+        <div class="become-member-container">
+            <div class="button call-to-action main-page-button  become-member">
+                <span>Devenez-membre</span>
+            </div>
         </div>
     </div>
-    <div class="become-member-container">
-        <div class="button call-to-action main-page-button  become-member">
-            <span>Devenez-member</span>
-        </div>
-    </div>
+
 </div>
 
 
